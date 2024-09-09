431ff54dfe304ffe3103f8590b034f907e0480c3
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-09-09 15:23:26 +0600

    sum of two number

---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2,total;
+    cout << "first number is ";
+    cin >> number1;
+    cout << "second number is ";
+    cin >> number2;
+    total = number1 + number2;
+    cout << "total is = " << total << endl;
+
+    return 0;
+}
